07def7fc770e04a0470daaea44831b9d46908018
karikai <kari.k.abraham@gmail.com>
2019-07-17 08:22:26 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,195 @@
+var students = [{"firstName":"Anna","lastName":"Garcia","gradYear":2022,"major":"Biology","school":"University of Connecticut"},{"firstName":"Adreanna","lastName":"Smith","gradYear":2021,"major":"Sociology ","school":"Central Connecticut State University"},{"firstName":"Alyssa","lastName":"Mesaros","gradYear":2020,"major":"Management Engineering ","school":"Worcester Polytechnic Institute "},{"firstName":"Neyalon ","lastName":"Vereen","gradYear":2021,"major":"Exercise science","school":"Central Connecticut State University"},{"firstName":"Nicole","lastName":"Michaels","gradYear":2019,"major":"Strategic Communication ","school":"Central Connecticut State University"},{"firstName":"Sarah","lastName":"Mwanza","gradYear":2020,"major":"Criminal Justice ","school":"Housatonic Community College"},{"firstName":"Cecil","lastName":"Pringle","gradYear":2020,"major":"Accounting","school":"University of Connecticut"},{"firstName":"Karen","lastName":"Guevara","gradYear":2020,"major":"Fine Arts","school":"Housatonic Community College"},{"firstName":"John","lastName":"Jensen","gradYear":2020,"major":"Marketing","school":"Central Connecticut State University"},{"firstName":"Dalton","lastName":"Everett","gradYear":2020,"major":"Communication","school":"Southern Connecticut State University"},{"firstName":"Nora","lastName":"Raccion","gradYear":2021,"major":"Education","school":"Southern Connecticut State University"},{"firstName":"Ciarra","lastName":"Jennings","gradYear":2021,"major":"Criminal Justice","school":"Housatonic Community College"},{"firstName":"daniel ","lastName":"reyes","gradYear":2022,"major":"dental higenist ","school":"Housatonic Community College"},{"firstName":"Diana ","lastName":"Muteba ","gradYear":2022,"major":"International Studies ","school":"Fairfield University"},{"firstName":"Jake","lastName":"Trauther","gradYear":2020,"major":"Computer Science","school":"Southern Connecticut State University"},{"firstName":"Olivia ","lastName":"Smith ","gradYear":2022,"major":"exercise science ","school":"Southern Connecticut State University"},{"firstName":"Haley","lastName":"Doll","gradYear":2021,"major":"Nursing","school":"Southern Connecticut State University"},{"firstName":"Justine","lastName":"Livingston","gradYear":2022,"major":"Healthcare Studies","school":"Southern Connecticut State University"},{"firstName":"Marcris","lastName":"Webb","gradYear":2020,"major":"Accounting ","school":"Central Connecticut State University"},{"firstName":"Evan","lastName":"Guadagno","gradYear":2019,"major":"Criminal Justice","school":"Housatonic Community College"},{"firstName":"pharaoh","lastName":"eaton","gradYear":2019,"major":"advanced manufacturing ","school":"Housatonic Community College"},{"firstName":"James","lastName":"Dotterman","gradYear":2020,"major":"Transfer Biology Studies","school":"Housatonic Community College"},{"firstName":"Xavier","lastName":"Larrea","gradYear":2022,"major":"General Studies","school":"Housatonic Community College"},{"firstName":"Catherine ","lastName":"Ruiz","gradYear":2020,"major":"Psychology","school":"Fairfield University"},{"firstName":"Natalie","lastName":"Sinclair","gradYear":2021,"major":"Public Health","school":"Southern Connecticut State University"},{"firstName":"Valerie ","lastName":"Bien-Aime ","gradYear":2020,"major":"Psychology ","school":"Fairfield University"},{"firstName":"Aja","lastName":"Jackson ","gradYear":2019,"major":"Exercise science ","school":"Central Connecticut State University"},{"firstName":"Jahzael","lastName":"Effend","gradYear":2020,"major":"Marketing ","school":"Central Connecticut State University"},{"firstName":"Elijah","lastName":"Blanco","gradYear":2021,"major":"Exercise science","school":"Central Connecticut State University"},{"firstName":"Shanaira","lastName":"Christie","gradYear":2018,"major":"Marriage and Family Therapy","school":"Southern Connecticut State University"},{"firstName":"Lauren","lastName":"Jones","gradYear":2022,"major":"Physics ","school":"New York University "},{"firstName":"Rachael","lastName":"Edlund","gradYear":2020,"major":"N/A","school":"Southern Connecticut State University"},{"firstName":"Justin","lastName":"Angeles","gradYear":2020,"major":"Management","school":"Central Connecticut State University"},{"firstName":"Victoria","lastName":"Pasion","gradYear":2020,"major":"Marketing","school":"Central Connecticut State University"},{"firstName":"Ayeisha ","lastName":"Preston ","gradYear":2020,"major":"Forensic Science ","school":"University of New Haven "},{"firstName":"Kenneth ","lastName":"Espinal","gradYear":2020,"major":"Mechanical engineering ","school":"Quinnipiac University "},{"firstName":"Leilanny","lastName":"Dagraca","gradYear":2020,"major":"Allied Health Sciences","school":"University of Connecticut"},{"firstName":"Brneducte","lastName":"Kabongo ","gradYear":2021,"major":"Buissness","school":"University of Connecticut"},{"firstName":"Luckario","lastName":"Alcide","gradYear":2021,"major":"Communications ","school":"Fairfield University"},{"firstName":"Hannah","lastName":"White","gradYear":2019,"major":"English","school":"Housatonic Community College"},{"firstName":"Alyah","lastName":"Fresnel","gradYear":2021,"major":"Psychology","school":"Central Connecticut State University"},{"firstName":"Bryn","lastName":"Claybourne","gradYear":2022,"major":"International Studies","school":"Central Connecticut State University"},{"firstName":"Kierra","lastName":"Palmer","gradYear":2019,"major":"Communications ","school":"Bryant University "},{"firstName":"Charles","lastName":"Mitchell","gradYear":2021,"major":"Business Management ","school":"Housatonic Community College"},{"firstName":"Monique","lastName":"Delacruz","gradYear":2022,"major":"General studies ","school":"University of Bridgeport "},{"firstName":"Robert","lastName":"Zurzola","gradYear":2022,"major":"Sport Management","school":"Southern Connecticut State University"},{"firstName":"Jasmin ","lastName":"Lugo ","gradYear":2023,"major":"criminal Justice ","school":"Housatonic Community College"},{"firstName":"fadi","lastName":"Gojani","gradYear":2019,"major":"Psychology","school":"Housatonic Community College"},{"firstName":"Isis","lastName":"DeLoatch","gradYear":2020,"major":"Social work ","school":"Central Connecticut State University"},{"firstName":"Whitney ","lastName":"Agyeman ","gradYear":2020,"major":"Sociology ","school":"Central Connecticut State University"},{"firstName":"Malik","lastName":"Jiggetts","gradYear":2021,"major":"Business Management","school":"University of Connecticut"},{"firstName":"Trinity","lastName":"McFadden ","gradYear":2022,"major":"Criminal Justice ","school":"Rochester Institute of Technology "},{"firstName":"Cynthia","lastName":"Moyano","gradYear":2021,"major":"International Studies ","school":"Fairfield University"},{"firstName":"Marissa","lastName":"Howlett","gradYear":2022,"major":"Biology","school":"Trinity College"},{"firstName":"Leila","lastName":"Larracuente ","gradYear":2019,"major":"Communication ","school":"Fairfield University"},{"firstName":"Diamond","lastName":"Pesante","gradYear":2023,"major":"Nursing","school":"University of Bridgeport "},{"firstName":"Onyinyechukwu ","lastName":"Nnodum ","gradYear":2021,"major":"Industrial and Labor Relations","school":"Cornell University "},{"firstName":"Greta","lastName":"Peleckaite","gradYear":2021,"major":"Marketing","school":"Central Connecticut State University"},{"firstName":"Aneri","lastName":"Patel","gradYear":2020,"major":"Exercise science ","school":"Central Connecticut State University"},{"firstName":"Andrea","lastName":"Fuentes","gradYear":2019,"major":"Communications and Media","school":"Fisher College "},{"firstName":"Joycelyn ","lastName":"Quaye","gradYear":2019,"major":"Management & Marketing ","school":"Central Connecticut State University"},{"firstName":"Casey","lastName":"McNally","gradYear":2020,"major":"Studio Art","school":"Southern Connecticut State University"},{"firstName":"Gabriella ","lastName":"Ferreira ","gradYear":2021,"major":"pre social work","school":"Central Connecticut State University"},{"firstName":"Kristen","lastName":"Gray","gradYear":2020,"major":"Education","school":"Southern Connecticut State University"},{"firstName":"Imani","lastName":"Quintana","gradYear":2021,"major":"Chemistry","school":"University of Connecticut"},{"firstName":"Chantel ","lastName":"Williams","gradYear":2020,"major":"Social Work ","school":"Western Connecticut State University "},{"firstName":"Denzel","lastName":"Akrofi","gradYear":2022,"major":"Bio","school":"Southern Connecticut State University"},{"firstName":"Nicholas","lastName":"Attah-Agyeman","gradYear":2019,"major":"Engineering","school":"Housatonic Community College"}]
+
+
+var schoolButton = document.getElementById('school');
+schoolButton.onclick = () => {
+    setSchoolChart();
+}
+
+var yearButton = document.getElementById('year');
+yearButton.onclick = () => {
+    setGradYearChart();
+}
+
+// School Chart
+    
+function getSchool() {
+    var doesCategoryExist;
+    var categoryReference;
+    var categories = [];
+
+    students.forEach(student => {
+        doesCategoryExist = false;
+        var newCategory = {
+            label: student.school.toString(),
+            values: [student]
+        }
+
+        categories.forEach(category => {
+            if (category.label === newCategory.label) {
+                doesCategoryExist = true;
+                categoryReference = category;
+            }
+        })
+
+        if (doesCategoryExist) {
+            categoryReference.values.push(student);
+        } else {
+            categories.push(newCategory);
+        }
+    })
+
+    return categories;
+}
+
+function getLabelsSchool() {    
+    const schoolArray = [];
+    getSchool().forEach(object => {
+        if (object.values.length > 5) {
+            schoolArray.push(object.label);
+        }
+    })
+    schoolArray[schoolArray.length] = 'Other';
+    return schoolArray;
+}
+
+function getValuesSchool() {
+    const schoolArray = [];
+    let other = 0;
+    getSchool().forEach(object => {
+        if (object.values.length < 5) {
+            other += object.values.length;
+        } else {
+            schoolArray.push(object.values.length);
+        }
+    })
+    schoolArray[schoolArray.length] = other;
+    return schoolArray;
+}
+
+function setSchoolChart() {
+    var ctx = 'myChart';
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: getLabelsSchool(),
+            datasets: [{
+                label: 'Students',
+                data: getValuesSchool(),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+// Graduation Year Chart
+
+function getGradYear() {
+    var doesCategoryExist;
+    var categoryReference;
+    var categories = [];
+
+    students.forEach(student => {
+        doesCategoryExist = false;
+        var newCategory = {
+            label: student.gradYear.toString(),
+            values: [student]
+        }
+        
+        categories.forEach(category => {
+            if (category.label === newCategory.label) {
+                doesCategoryExist = true;
+                categoryReference = category;
+            }
+        })
+
+        if (doesCategoryExist) {
+            categoryReference.values.push(student);
+        } else {
+            categories.push(newCategory);
+        }
+    })
+
+    return categories;
+}
+
+function getLabelsGradYear() {
+    const gradYearArray = [];
+    getGradYear().forEach(object => {
+        gradYearArray.push(object.label);
+    })
+    return gradYearArray;
+}
+
+function getValuesGradYear() {
+    const gradYearArray = [];
+    getGradYear().forEach(object => {
+        gradYearArray.push(object.values.length);
+    })
+    return gradYearArray;
+}
+
+function setGradYearChart() {
+    var ctx = 'myChart';
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: getLabelsGradYear(),
+            datasets: [{
+                label: '# of Students by Graduation Year',
+                data: getValuesGradYear(),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
